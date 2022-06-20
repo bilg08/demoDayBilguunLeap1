89b62de8c4e5178b937303ddc514a066927fe500
bilguunZorigooLeap1 <bilguunzorigooleap1@iMac2021-02s-iMac.local>
2022-06-20 11:45:59 +0800

    commit

--- modBurtgeljuulehHeseg/modBurtgel.js
+++ modBurtgeljuulehHeseg/modBurtgel.js
@@ -1,4 +1,60 @@
-let id=0;
+
+///////firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
+  import {
+   getStorage,ref as sRef,
+   uploadBytes ,
+   uploadBytesResumable
+    } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-storage.js";
+
+  import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  getDoc,
+  setDoc,
+  doc,
+  updateDoc,
+  increment,
+  } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
+  import {
+  getDatabase,
+  onValue,
+  ref,
+  push,
+  query,
+  orderByChild,
+  orderByKey,
+  child,
+  set,
+  get,
+  onChildChanged,
+  } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyDgYAy6FkKAJ2_0e07Xh8LhzYOpP1ra9Q8",
+    authDomain: "demo1-1ee9d.firebaseapp.com",
+    databaseURL: "https://demo1-1ee9d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "demo1-1ee9d",
+    storageBucket: "demo1-1ee9d.appspot.com",
+    messagingSenderId: "247353066341",
+    appId: "1:247353066341:web:4b8f133a68a3d85679578c",
+    measurementId: "G-5N6BTMR62T"
+  };
+      const app = initializeApp(firebaseConfig);
+      const auth=getAuth(app);
+      const db=getFirestore(app);
+      const realDb=getDatabase(app);
+      const storage = getStorage(app);
+ 
 const signUp = document.getElementById("signUp");
 const signIn = document.getElementById("signIn");
 const advise = document.getElementById("advise");
@@ -8,76 +64,30 @@ const tt = document.getElementsByClassName("otherTree")[0];
 console.log("hello")
 let inputValue="";
 
-//  sing up garj ireh
-
-function show1(){
-const modal = document.getElementsByClassName("signUp-modal")[0];
-console.log("j",modal);
-modal.style.display= 'block';
-}
-
-function toggle1(e){          
-console.log("toggle", e);
-e.parentElement.style.display = 'none'; 
-}
-
-// sign in garj ireh
 
 
-function show2(){
-const modal = document.getElementsByClassName("signIn-modal")[0];
-console.log("jj", modal);
-modal.style.display = 'block';
-}
-function toggle2(e){
-console.log("toggle2", e);
-e.parentElement.style.display = 'none'; 
-}
 
-// advise garj ireh 
-
-function show3(){
-const modal = document.getElementsByClassName("advise-modal")[0];
-console.log("jjj", modal);
-modal.style.display = 'block';
-}
-function toggle3(e){
-console.log("toggle3", e);
-e.parentElement.style.display = 'none'; 
-}
-
-//statistic garj ireh
-
-function show4(){
-const modal=document.getElementsByClassName("statistic-modal")[0];
-console.log("jjjj", modal);
-modal.style.display = 'block';
-}
-function toggle4(e){
-console.log("toggle3", e);
-e.parentElement.style.display = 'none'; 
-}
 
 // on car udur
 
-const date_picker_element = document.querySelector('.date-picker');
-const selected_date_element = document.querySelector('.date-picker .selected-date');
-const dates_element=document.querySelector('.date-picker .dates');
-const mth_element=document.querySelector('.date-picker .dates .month .mth');
-const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
-const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
-const days_element = document.querySelector('.date-picker .dates .days');
-const months=['1 cap','2 cap','3 cap','4 cap','5 cap','6 cap','7 cap','8 cap','9 cap','10 cap','11 cap','12 cap',];
+        const date_picker_element = document.querySelector('.date-picker');
+        const selected_date_element = document.querySelector('.date-picker .selected-date');
+        const dates_element=document.querySelector('.date-picker .dates');
+        const mth_element=document.querySelector('.date-picker .dates .month .mth');
+        const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
+        const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
+        const days_element = document.querySelector('.date-picker .dates .days');
+        const months=['1 cap','2 cap','3 cap','4 cap','5 cap','6 cap','7 cap','8 cap','9 cap','10 cap','11 cap','12 cap',];
 
 let date = new Date();
 let day = date.getDate();
 let month = date. getMonth();
 let year = date. getFullYear();
 
-let selectedDate = date;
-let selectedDay =day;
-let selectedMonth =month;
-let selectedYear = year;
+        let selectedDate = date;
+        let selectedDay =day;
+        let selectedMonth =month;
+        let selectedYear = year;
 
 mth_element.textContent = months[month] + " " + year;
 console.log(
@@ -86,11 +96,11 @@ mth_element.textContent = months[month] + " " + year)
 selected_date_element.textContent=formatDate(date);
 selected_date_element.dataset.value=selectedDate;
 
-populateDates();
-date_picker_element.addEventListener('click',toggleDatePicker);
-next_mth_element.addEventListener('click', goToNextMonth);
-prev_mth_element.addEventListener('click',goToPrevMonth);
-function toggleDatePicker (e){
+      populateDates();
+      date_picker_element.addEventListener('click',toggleDatePicker);
+      next_mth_element.addEventListener('click', goToNextMonth);
+      prev_mth_element.addEventListener('click',goToPrevMonth);
+      function toggleDatePicker (e){
 
 if(!checkEventPathForClass(e.path,'dates')){
   dates_element.classList.toggle('active');
@@ -174,72 +184,70 @@ let r;
 let too=document.getElementById("countmod")
 
 document.getElementById("addBtn").addEventListener("click",async function(){
- console.log("add button daragdlaa.",document.getElementById("SongogdsonModGargahHeseg"));
+    console.log("add button daragdlaa.",document.getElementById("SongogdsonModGargahHeseg"));
 
- let li = document.createElement("li");
- if(document.getElementById("modniitorol").value === "бусад"){
-  inputValue = document.getElementById("ooa").value;
- }else {
-  inputValue = document.getElementById("modniitorol").value
- }
-  console.log(inputValue);
- let inputCount=document.getElementById("counttree").value;
-  let t = document.createTextNode(inputValue);
-  let span2 = document.createElement("SPAN")
-  let span1 = document.createElement("SPAN");
-  let span = document.createElement("SPAN");
+    let li = document.createElement("li");
+    if(document.getElementById("modniitorol").value === "бусад"){
+      inputValue = document.getElementById("ooa").value;
+    }else {
+      inputValue = document.getElementById("modniitorol").value
+    }
+      console.log(inputValue);
+    let inputCount=document.getElementById("counttree").value;
+      let t = document.createTextNode(inputValue);
+      let span2 = document.createElement("SPAN")
+      let span1 = document.createElement("SPAN");
+      let span = document.createElement("SPAN");
 
-  let txt = document.createTextNode("\u00D7");
- 
-  let tiim =document.createTextNode(" \u221A")
-  let dots=document.createTextNode(inputCount)
-  console.log(inputCount)
-
-  li.appendChild(t);
-  if (inputCount == 0 || inputCount < 0) {
-    alert("Тоогоо оруулна уу!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-    li.appendChild(span2)
-    li.appendChild(span1)
-    li.appendChild(span);
-   
-    span.appendChild(txt)
-    span1.appendChild(dots)
-    span2.appendChild(t)
-
-    span2.className="modniiTorolNer"
-    span1.className="tooShirheg"
-    span.className = "test";
-  
-   
-    li.className="list";
+      let txt = document.createTextNode("\u00D7");
     
-  }
-  let sum=0;
-  for(let i of close){
-      sum+=parseInt(i.parentElement.getElementsByClassName("tooShirheg")[0].innerText);
-      i.addEventListener('click', ()=>{
-      sum-= parseInt(i.parentElement.getElementsByClassName("tooShirheg")[0].innerText);
-      i.parentElement.remove();
-      updateSum(sum);
-      });
-   }
-  updateSum(sum);
-  document.getElementById("counttree").placeholder
+      let tiim =document.createTextNode(" \u221A")
+      let dots=document.createTextNode(inputCount)
+      console.log(inputCount)
 
-});
-function updateSum(sum){
-  if(sum>0){
+      li.appendChild(t);
+      if (inputCount == 0 || inputCount < 0) {
+      alert("Тоогоо оруулна уу!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+      li.appendChild(span2)
+      li.appendChild(span1)
+      li.appendChild(span);
+    
+      span.appendChild(txt)
+      span1.appendChild(dots)
+      span2.appendChild(t)
+
+      span2.className="modniiTorolNer"
+      span1.className="tooShirheg"
+      span.className = "test";
+    
+    
+      li.className="list";
+      
+    }
+    let sum=0;
+    for(let i of close){
+        sum+=parseInt(i.parentElement.getElementsByClassName("tooShirheg")[0].innerText);
+        i.addEventListener('click', ()=>{
+        sum-= parseInt(i.parentElement.getElementsByClassName("tooShirheg")[0].innerText);
+        i.parentElement.remove();
+        updateSum(sum);
+        });
+    }
+    updateSum(sum);
+    document.getElementById("counttree").placeholder
+
+  });
+  function updateSum(sum){
+    if(sum>0){
   
-      document.getElementById("ModniiContainer").style.display = "block";
-  }else{
-      document.getElementById("ModniiContainer").style.display = "none";
+        document.getElementById("ModniiContainer").style.display = "block";
+    }else{
+        document.getElementById("ModniiContainer").style.display = "none";
+    }
+    too.innerText = sum;
   }
-  too.innerText = sum;
-}
-//    let close = document.querySelectorAll('.test');
-//    console.log("x:", close);    
 
 
 function selectItem(el){
@@ -252,54 +260,9 @@ if(el.value === "бусад"){
 }
 
 
-///////firebase
-import {
-getAuth,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut,
-onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
-import {
-getFirestore,
-collection,
-getDocs,
-addDoc,
-getDoc,
-setDoc,
-doc,
-updateDoc,
-increment,
-} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
-import {
-getDatabase,
-onValue,
-ref,
-push,
-query,
-orderByChild,
-orderByKey,
-child,
-set,
-get,
-onChildChanged,
-} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
 
-const firebaseConfig = {
-apiKey: "AIzaSyDgYAy6FkKAJ2_0e07Xh8LhzYOpP1ra9Q8",
-authDomain: "demo1-1ee9d.firebaseapp.com",
-databaseURL: "https://demo1-1ee9d-default-rtdb.asia-southeast1.firebasedatabase.app",
-projectId: "demo1-1ee9d",
-storageBucket: "demo1-1ee9d.appspot.com",
-messagingSenderId: "247353066341",
-appId: "1:247353066341:web:4b8f133a68a3d85679578c",
-measurementId: "G-5N6BTMR62T"
-};
-const app = initializeApp(firebaseConfig);
-const auth=getAuth(app);
-const db=getFirestore(app);
-const realDb=getDatabase(app);
+
+
 
 // ********************************************************************************************************** //
 onAuthStateChanged(auth,async(user)=>{
@@ -362,14 +325,14 @@ document.getElementById("registrationButton").addEventListener("click",async()=>
       //Тухайн төрөл модны тоо ширхэг
       const torolModniiToo=document.getElementsByClassName("tooShirheg");
       // console.log(listModHaruulah,listModniiNer);
-  let bairshil=document.getElementById('bairshil').value;
-  // Бүх модны нийлбэр
-  let tooShirheg=parseInt(document.getElementById('countmod').innerText);
-  let HezeeTarisan=document.getElementsByClassName('selected-date')[0].innerText;
-  let modniitorol=document.getElementById('modniitorol').value;
-  let modniiiDelgerengui=document.getElementById("description").value;
-      console.log(bairshil,tooShirheg,HezeeTarisan,modniitorol,modniiiDelgerengui);
-      
+        let bairshil=document.getElementById('bairshil').value;
+        // Бүх модны нийлбэр
+        let tooShirheg=parseInt(document.getElementById('countmod').innerText);
+        let HezeeTarisan=document.getElementsByClassName('selected-date')[0].innerText;
+        let modniitorol=document.getElementById('modniitorol').value;
+        let modniiiDelgerengui=document.getElementById("description").value;
+            console.log(bairshil,tooShirheg,HezeeTarisan,modniitorol,modniiiDelgerengui);
+            
 
 
       onAuthStateChanged(auth,async(user)=>{
@@ -579,12 +542,32 @@ document.getElementById("registrationButton").addEventListener("click",async()=>
                                       })
                                       location.reload();
               }
-          })
-          ///////baiguulagaeseh        
+          })  
+          
           }
-          ////if(user)
+
           }
 });
-////ounauth
 
+
+})
+
+const saveBtn=document.getElementById("save");
+// files.addEventListener("click",saveFile);
+saveBtn.addEventListener("click",()=>{
+  console.log("helo")
+  const filee=document.getElementById("image");
+ 
+    // 'file' comes from the Blob or File API
+    if(filee.files[0]){
+      console.log(filee.files[0]);
+      const storageRef = sRef(storage, auth.currentUser.uid+"/"+ filee.files[0].name ,);
+        console.log("Saving...", filee.files[0]);
+      uploadBytes(storageRef, filee.files[0],).then((snapshot) => {
+       
+        console.log("Saved");
+      }).catch(err=>console.log(err));
+    }else{
+      alert("zurag oruulna uu")
+    }
 })
